96906c6c817c02d370615ce1f150243e6fd2a499
Josh Burks <dotnofoolin@gmail.com>
2019-04-22 17:15:28 -0500

    Add bulma and fontawesome.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
