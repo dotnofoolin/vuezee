c784411f4d6bd8017489df4f875de3da83aea914
Josh Burks <dotnofoolin@gmail.com>
2019-04-25 13:47:01 -0500

    Move Title, Howto, and About code to separate component.

---
+++ src/components/TitleHowtoAbout.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="title">
+    <i class="fas fa-dice fancy-title-dice has-text-info"></i>
+    <div class="fancy-title has-text-info">Vuezee</div>
+    <br>
+    <a class="button is-text" @click="show_howto_modal = true">How To</a>
+    <a class="button is-text" @click="show_about_modal = true">About</a>
+    <div class="modal" :class="[{ 'is-active': show_howto_modal }]">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <div class="content has-text-left">
+            <p>How To Play Vuezee</p>
+              <p>
+                <ol>
+                  <li>Click the roll button (spinning dice) to begin your first roll.</li>
+                  <li>Click any dice you want to keep.</li>
+                  <li>You have 3 rolls to try and make scoreable combos.</li>
+                  <li>Click the line in the scorecard you wish to score your dice.</li>
+                  <li>Repeat until you've completed the scorecard.</li>
+                </ol>
+              </p>
+          </div>
+          <button class="button is-info" @click="show_howto_modal = false">Close</button>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="show_howto_modal = false"></button>
+    </div>
+    <div class="modal" :class="[{ 'is-active': show_about_modal }]">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <div class="content">
+            <p>Vuezee is a Yahtzee clone created with Vue.js, Bulma, and FontAwesome.</p>
+            <hr>
+            <p>Created by Josh Burks</p>
+            <a href="https://github.com/dotnofoolin/vuezee" target="_blank">
+              https://github.com/dotnofoolin/vuezee
+            </a>
+          </div>
+          <button class="button is-info" @click="show_about_modal = false">Close</button>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="show_about_modal = false"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TitleHowtoAbout',
+    data () {
+      return {
+        show_about_modal: false,
+        show_howto_modal: false
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .fancy-title-dice {
+    font-size: 5em;
+  }
+
+  .fancy-title {
+    font-family: 'Pacifico', cursive;
+    font-size: 6em;
+    transform-origin: center;
+    transform: rotate(-15deg);
+  }
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,11 +5,7 @@
       <div class="tile is-ancestor">
         <div class="tile is-parent is-4 is-vertical">
           <div class="tile is-child box">
-            <i class="fas fa-dice fancy-title-dice has-text-info"></i>
-            <div class="fancy-title has-text-info">Vuezee</div>
-            <br>
-            <a class="button is-text" @click="show_howto_modal = true">How To</a>
-            <a class="button is-text" @click="show_about_modal = true">About</a>
+            <title-howto-about></title-howto-about>
           </div>
           <div class="tile is-child box">
             <p class="is-size-5">{{ rollText() }}</p>
@@ -91,53 +87,27 @@
         </div>
       </div>
     </div>
-    <div class="modal" :class="[{ 'is-active': show_howto_modal }]">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-        <div class="box">
-          <div class="content has-text-left">
-            <p>How To Play Vuezee</p>
-              <p>
-                <ol>
-                  <li>Click the Roll button (spinning dice) to begin your first roll.</li>
-                  <li>You have 3 rolls to try and make scoreable combos.</li>
-                  <li>Click the line in the scorecard you wish to score your dice.</li>
-                  <li>Repeat until you've completed the scorecard.</li>
-                </ol>
-              </p>
-          </div>
-          <button class="button is-info" @click="show_howto_modal = false">Close</button>
-        </div>
-      </div>
-      <button class="modal-close is-large" aria-label="close" @click="show_howto_modal = false"></button>
-    </div>
-    <div class="modal" :class="[{ 'is-active': show_about_modal }]">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-        <div class="box">
-          <div class="content">
-            <p>Vuezee is a Yahtzee clone created with Vue.js, Bulma, and FontAwesome.</p>
-            <hr>
-            <p>Created by Josh Burks</p>
-            <a href="https://github.com/dotnofoolin/vuezee" target="_blank">
-              https://github.com/dotnofoolin/vuezee
-            </a>
-          </div>
-          <button class="button is-info" @click="show_about_modal = false">Close</button>
-        </div>
-      </div>
-      <button class="modal-close is-large" aria-label="close" @click="show_about_modal = false"></button>
-    </div>
   </div>
 </template>
 
 <script>
+  /* TODO
+      - move things to components (like highscore tiles)
+      - move dice and scorecard to Vuex (maybe not dice)
+      - reset game/scorecard
+      - log highscores
+      - prevent playing on vuezee if it was scored 0
+  */
+
+  import TitleHowtoAbout from '@/components/TitleHowtoAbout'
+
   export default {
     name: 'game',
+    components: {
+      'title-howto-about': TitleHowtoAbout
+    },
     data () {
       return {
-        show_about_modal: false,
-        show_howto_modal: false,
         roll_count: 0,
         all_dice: [
           {
