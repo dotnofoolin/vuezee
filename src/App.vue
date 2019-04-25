a3d8c33f715ae50828fdf26a3afb6c8dd51e0359
Josh Burks <dotnofoolin@gmail.com>
2019-04-25 13:46:16 -0500

    Tweak the fonts and other styles a bit.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 </template>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Pacifico');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
   @import './assets/styles.scss';
   @import '~bulma/bulma.sass';
-  @import url('https://fonts.googleapis.com/css?family=Pacifico');
 </style>
